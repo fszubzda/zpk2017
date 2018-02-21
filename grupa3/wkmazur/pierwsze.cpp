ca0537216e1e0889dfc6c745494cee88168e99ad
wkmazur <36708515+wkmazur@users.noreply.github.com>
2018-02-21 19:53:01 +0100

    Create pierwsze.cpp

---
+++ grupa3/wkmazur/pierwsze.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int p=0;
+
+    cin>> n;
+
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            p++;
+        }
+    }
+
+    if(p==0){
+        cout << "pierwsza" << endl;
+    }
+    else {
+        cout << "zlozona" << endl;
+    }
+
+    return 0;
+}
